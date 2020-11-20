b50bd810dc1979470c19ca7c37246a5af917691e
bdvries <bdvries@sogyo.nl>
2020-11-20 12:04:16 +0100

    separated test and refactored according naming convention

--- src/test/java/nl/sogyo/mancala/domain/PlayerTest.java
+++ src/test/java/nl/sogyo/mancala/domain/PlayerTest.java
@@ -5,12 +5,25 @@ import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
   @Test
-  public void createPlayers(){
+  public void Player_NewPlayer_PlayerAndOpponentCreated(){
     Player player1 = new Player();
 
     assertNotNull(player1.getOpponent(), "The opponent was not created");
+  }
+
+  @Test
+  public void Player_NewPlayer_PlayerHasTurnAndOpponentDoesNot(){
+    Player player1 = new Player();
+
     assertTrue(player1.getHasTurn(), "player1 one should have the turn");
     assertFalse(player1.getOpponent().getHasTurn(), "The opponent of player1 should not have the turn");
+    assertSame(player1, player1.getOpponent().getOpponent(), "The opponent of the opponent of player1 should be player1. There is no proper reference between the players.");
+  }
+
+  @Test
+  public void Player_NewPlayer_PlayerAndOpponentAreEachothersOpponent(){
+    Player player1 = new Player();
+
     assertSame(player1, player1.getOpponent().getOpponent(), "The opponent of the opponent of player1 should be player1. There is no proper reference between the players.");
   }
 }
