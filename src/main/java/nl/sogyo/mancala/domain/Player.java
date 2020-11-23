0345d7c50546f4b5ac12a548f0e2ae5436616e0d
bdvries <bdvries@sogyo.nl>
2020-11-23 13:20:06 +0100

    Player now has a sequence of containers when created

--- src/main/java/nl/sogyo/mancala/domain/Player.java
+++ src/main/java/nl/sogyo/mancala/domain/Player.java
@@ -3,10 +3,12 @@ package nl.sogyo.mancala.domain;
 public class Player {
   private Player opponent;
   private Boolean hasTurn;
+  private Pit firstPit;
 
   public Player(){
     this.hasTurn = true;
     this.opponent = new Player(this);
+    this.firstPit = new Pit(6, this);
   }
 
   public Player(Player opponent){
@@ -23,6 +25,6 @@ public class Player {
   }
 
   public Pit getFirstPit(){
-    return null;
+    return this.firstPit;
   }
 }
