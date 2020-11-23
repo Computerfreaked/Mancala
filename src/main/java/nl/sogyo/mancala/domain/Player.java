8067e19ad690fb7e90f747dabd8d7e3d8e695eab
bdvries <bdvries@sogyo.nl>
2020-11-23 13:26:39 +0100

    opponent now properly creates a sequence of containers

--- src/main/java/nl/sogyo/mancala/domain/Player.java
+++ src/main/java/nl/sogyo/mancala/domain/Player.java
@@ -14,6 +14,7 @@ public class Player {
   public Player(Player opponent){
     this.hasTurn = false;
     this.opponent = opponent;
+    this.firstPit = new Pit(6, this);
   }
 
   public Player getOpponent(){
