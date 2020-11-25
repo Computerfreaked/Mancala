8d71506efdbdd74a545a600d79c38d8f9261b1a7
bdvries <bdvries@sogyo.nl>
2020-11-25 10:50:45 +0100

    removed unused constructors in new setup

--- src/main/java/nl/sogyo/mancala/domain/Kalaha.java
+++ src/main/java/nl/sogyo/mancala/domain/Kalaha.java
@@ -1,15 +1,6 @@
 package nl.sogyo.mancala.domain;
 
 public class Kalaha extends Container{
-  public Kalaha(){
-    //default but just to be sure
-    this.amountStones = 0;
-  }
-
-  public Kalaha(Player player){
-    this.owner = player;
-  }
-
   public Kalaha(int amount, Player player){
     this.amountStones = 0;
 
