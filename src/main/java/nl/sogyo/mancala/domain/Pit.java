8d13b83341420b2dc5cb35823a6e9f27babef7e1
bdvries <bdvries@sogyo.nl>
2020-11-25 10:48:10 +0100

    removed unused contructor in new setup

--- src/main/java/nl/sogyo/mancala/domain/Pit.java
+++ src/main/java/nl/sogyo/mancala/domain/Pit.java
@@ -33,29 +33,6 @@ public class Pit extends Container{
 
   }
 
-  public Pit(int amount){
-    this.amountStones = 4;
-
-    if(amount > 1){
-      this.nextContainer = new Pit(amount - 1);
-    }
-    else{
-      this.nextContainer = new Kalaha();
-    }
-  }
-
-  public Pit(int amount, Player player) {
-    this.amountStones = 4;
-    this.owner = player;
-
-    if(amount > 1){
-      this.nextContainer = new Pit(amount - 1, player);
-    }
-    else{
-      this.nextContainer = new Kalaha(player);
-    }
-  }
-
   public void play() {
     if(this.amountStones == 0){
       return;
