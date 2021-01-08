113ba19bc5d0e681c0d796d7abb5f031d2ffcc16
bdvries <bdvries@sogyo.nl>
2021-01-08 10:44:11 +0100

    test

--- versionBumper.sh
+++ versionBumper.sh
@@ -3,3 +3,5 @@
 readarray -t -d '.' versionParts <<< $(git describe --abbrev=0)
 newVersion=${versionParts[0]}.$((${versionParts[1]} + 1)).${versionParts[2]}
 echo -e "\033[0;32mThe new version number is "$newVersion
+echo "CI commit branch: "$CI_COMMIT_BRANCH
+echo "CI pipeline source :"$CI_PIPELINE_SOURCE
