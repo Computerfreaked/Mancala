ca0931e7495bd7ddbd7219715e8f4606cc4e1864
bdvries <bdvries@sogyo.nl>
2021-01-08 10:56:14 +0100

    merged bump and get latest version

--- versionBumper.sh
+++ versionBumper.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-readarray -t -d '.' versionParts <<< $(git describe --abbrev=0)
-newVersion=${versionParts[0]}.$((${versionParts[1]} + 1)).${versionParts[2]}
-echo -e "\033[0;32mThe new version number is "$newVersion
-echo "CI commit branch: "$CI_COMMIT_BRANCH
-echo "CI pipeline source :"$CI_PIPELINE_SOURCE
+if [ $CI_COMMIT_BRANCH == "master" && $CI_PIPELINE_SOURCE == "merge_request_event" ]; then
+  readarray -t -d '.' versionParts <<< $(git describe --abbrev=0)
+  newVersion=${versionParts[0]}.$((${versionParts[1]} + 1)).${versionParts[2]}
+  echo -e "\033[0;32mThe new version number is "$newVersion
+fi
+
+mkdir target
+latestVersion=$(git describe --always | tee target/currentVersion.txt)
+mvn versions:set -DnewVersion=$latestVersion
