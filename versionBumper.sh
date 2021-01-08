0373ec26408ef6d8c9e168a80234bdfbfc8a3794
bdvries <bdvries@sogyo.nl>
2021-01-08 11:01:39 +0100

    fixed script

--- versionBumper.sh
+++ versionBumper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $CI_COMMIT_BRANCH == "master" && $CI_PIPELINE_SOURCE == "merge_request_event" ]; then
+if [[ $CI_COMMIT_BRANCH == "master" && $CI_PIPELINE_SOURCE == "merge_request_event" ]]; then
   readarray -t -d '.' versionParts <<< $(git describe --abbrev=0)
   newVersion=${versionParts[0]}.$((${versionParts[1]} + 1)).${versionParts[2]}
   echo -e "\033[0;32mThe new version number is "$newVersion
