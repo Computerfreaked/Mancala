9b8c7cebe655702a6464b5f2a8ec7ea5e87225b5
bdvries <bdvries@sogyo.nl>
2021-01-08 11:15:25 +0100

    added git username email

--- versionBumper.sh
+++ versionBumper.sh
@@ -4,6 +4,9 @@
   readarray -t -d '.' versionParts <<< $(git describe --abbrev=0)
   newVersion=${versionParts[0]}.$((${versionParts[1]} + 1)).${versionParts[2]}
   echo -e "\033[0;32mThe new version number is "$newVersion"\033[0m"
+
+  git config user.name "CICD Version tagging"
+  git config user.email "bdvries@sogyo.nl"
   git tag -a $newVersion -m "New feature"
 #fi
 
